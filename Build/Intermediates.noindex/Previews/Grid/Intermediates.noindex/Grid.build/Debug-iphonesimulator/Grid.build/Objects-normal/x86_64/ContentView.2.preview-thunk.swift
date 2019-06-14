5248c83b43682009559259735f650c95a7e82883
chrislongden <chris@clickstoapps.co.uk>
2019-06-14 10:58:39 +0100

    Tweaking

--- Build/Intermediates.noindex/Previews/Grid/Intermediates.noindex/Grid.build/Debug-iphonesimulator/Grid.build/Objects-normal/x86_64/ContentView.2.preview-thunk.swift
+++ Build/Intermediates.noindex/Previews/Grid/Intermediates.noindex/Grid.build/Debug-iphonesimulator/Grid.build/Objects-normal/x86_64/ContentView.2.preview-thunk.swift
@@ -4,7 +4,7 @@ import SwiftUI
 #if DEBUG
 extension ContentView_Previews {
     @_dynamicReplacement(for: previews) private static var __preview__previews: some View {
-        #sourceLocation(file: "/Users/chrislongden/Dropbox/Apps-Personal/iOS/Grid/Grid/ContentView.swift", line: 75)
+        #sourceLocation(file: "/Users/chrislongden/Dropbox/Apps-Personal/iOS/Grid/Grid/ContentView.swift", line: 83)
         AnyView(ContentView())
 #sourceLocation()
     }
@@ -13,44 +13,47 @@ extension ContentView_Previews {
 
 extension GridView {
     @_dynamicReplacement(for: body) private var __preview__body: some View {
-        #sourceLocation(file: "/Users/chrislongden/Dropbox/Apps-Personal/iOS/Grid/Grid/ContentView.swift", line: 47)
+        #sourceLocation(file: "/Users/chrislongden/Dropbox/Apps-Personal/iOS/Grid/Grid/ContentView.swift", line: 51)
+        
         AnyView(VStack(alignment: .center, spacing: self.spacing) {
+            
             ForEach(0..<rowCount) { rowIndex in
+                
                 HStack (alignment: .center, spacing: self.spacing) {
+                    
                     ForEach((rowIndex * self.columns)..<(rowIndex * self.columns) + self.columns) { itemIndex in
                         if itemIndex >= self.content.data.count {
-                            Text(__designTimeString("#1110.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].arg[2].value.[0].arg[1].value.[0].[0].[0].arg[0].value", fallback: ""))
-                                .frame(minWidth: __designTimeInteger("#1110.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].arg[2].value.[0].arg[1].value.[0].[0].[0].modifier[0].arg[0].value", fallback: 0), maxWidth: .infinity,
-                                       minHeight: __designTimeInteger("#1110.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].arg[2].value.[0].arg[1].value.[0].[0].[0].modifier[0].arg[2].value", fallback: 0), maxHeight: .infinity)
+                            Text(__designTimeString("#8241.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].arg[2].value.[0].arg[1].value.[0].[0].[0].arg[0].value", fallback: ""))
+                                .frame(minWidth: __designTimeInteger("#8241.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].arg[2].value.[0].arg[1].value.[0].[0].[0].modifier[0].arg[0].value", fallback: 0), maxWidth: .infinity, minHeight: __designTimeInteger("#8241.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].arg[2].value.[0].arg[1].value.[0].[0].[0].modifier[0].arg[2].value", fallback: 0), maxHeight: .infinity)
                             
                         } else {
                             self.content.content(itemIndex)
-                                .frame(minWidth: __designTimeInteger("#1110.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].arg[2].value.[0].arg[1].value.[0].[1].[0].modifier[1].arg[0].value", fallback: 0), maxWidth: .infinity,
-                                       minHeight: __designTimeInteger("#1110.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].arg[2].value.[0].arg[1].value.[0].[1].[0].modifier[1].arg[2].value", fallback: 0), maxHeight: .infinity)
+                                .frame(minWidth: __designTimeInteger("#8241.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].arg[2].value.[0].arg[1].value.[0].[1].[0].modifier[1].arg[0].value", fallback: 0), maxWidth: .infinity, minHeight: __designTimeInteger("#8241.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].arg[2].value.[0].arg[1].value.[0].[1].[0].modifier[1].arg[2].value", fallback: 0), maxHeight: .infinity)
                         }
                     }
-                }.frame(minWidth: __designTimeInteger("#1110.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].modifier[0].arg[0].value", fallback: 0), maxWidth: .infinity,
-                        minHeight: __designTimeInteger("#1110.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].modifier[0].arg[2].value", fallback: 0), maxHeight: .infinity)
+                    
+                }.frame(minWidth: __designTimeInteger("#8241.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].modifier[0].arg[0].value", fallback: 0), maxWidth: .infinity, minHeight: __designTimeInteger("#8241.[2].[5].property.[0].[0].arg[2].value.[0].arg[1].value.[0].modifier[0].arg[2].value", fallback: 0), maxHeight: .infinity)
+                
             }
+            
         }
-        .frame(minWidth:__designTimeInteger("#1110.[2].[5].property.[0].[0].modifier[0].arg[0].value", fallback: 0), maxWidth: .infinity,
-               minHeight:__designTimeInteger("#1110.[2].[5].property.[0].[0].modifier[0].arg[2].value", fallback: 0), maxHeight: .infinity))
+        .frame(minWidth:__designTimeInteger("#8241.[2].[5].property.[0].[0].modifier[0].arg[0].value", fallback: 0), maxWidth: .infinity, minHeight:__designTimeInteger("#8241.[2].[5].property.[0].[0].modifier[0].arg[2].value", fallback: 0), maxHeight: .infinity))
 #sourceLocation()
     }
 }
 
 extension ContentView {
     @_dynamicReplacement(for: body) private var __preview__body: some View {
-        #sourceLocation(file: "/Users/chrislongden/Dropbox/Apps-Personal/iOS/Grid/Grid/ContentView.swift", line: 14)
+        #sourceLocation(file: "/Users/chrislongden/Dropbox/Apps-Personal/iOS/Grid/Grid/ContentView.swift", line: 15)
         
-        AnyView(GridView (columns: __designTimeInteger("#1110.[1].[0].property.[0].[0].arg[0].value", fallback: 4)) {
-            ForEach(0...13) { number in
-                Text("Hello \(number)")
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(Color.white)
+        AnyView(GridView (columns: __designTimeInteger("#8241.[1].[0].property.[0].[0].arg[0].value", fallback: 3), spacing: __designTimeInteger("#8241.[1].[0].property.[0].[0].arg[1].value", fallback: 5)) {
+            ForEach(1...11) { number in
+                Text("\(number+1)")
+                    .font(.largeTitle)
+                    .frame(minWidth: __designTimeInteger("#8241.[1].[0].property.[0].[0].arg[2].value.[0].arg[1].value.[0].modifier[1].arg[0].value", fallback: 0), maxWidth: .infinity, minHeight: __designTimeInteger("#8241.[1].[0].property.[0].[0].arg[2].value.[0].arg[1].value.[0].modifier[1].arg[2].value", fallback: 0), maxHeight: .infinity)
+                    .background(Color.green)
             }
-        })
+        }.padding())
 #sourceLocation()
     }
 }
--- Grid/ContentView.swift
+++ Grid/ContentView.swift
@@ -9,16 +9,17 @@
 import SwiftUI
 
 struct ContentView : View {
+    
     var body: some View {
         
-        GridView (columns: 4) {
-            ForEach(0...13) { number in
-                Text("Hello \(number)")
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(Color.white)
+        GridView (columns: 3, spacing: 5) {
+            ForEach(1...11) { number in
+                Text("\(number+1)")
+                    .font(.largeTitle)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(Color.green)
             }
-        }
+        }.padding()
         
     }
 }
@@ -30,43 +31,48 @@ struct GridView<Content> : View where Content:View {
     private var content: ForEach<ClosedRange<Int>, Content>
     private var rowCount:Int
     
-    @inlinable init(columns:Int, spacing:Length = 0, @ViewBuilder content: () -> ForEach<ClosedRange<Int>, Content>) {
+    @inlinable init(columns:Int,
+                    spacing:Length = 0,
+                    @ViewBuilder content: () -> ForEach<ClosedRange<Int>, Content>) {
+        
         self.columns = columns
         self.spacing = spacing
         self.content = content()
     
         // Calculate row count based on items vs columns and remaining items
-        rowCount = (max(self.content.data.count, columns) / columns) +
-            ((self.content.data.count % columns) != 0 ? 1 : 0)
+        rowCount = (max(self.content.data.count, columns) / columns) + ((self.content.data.count % columns) != 0 ? 1 : 0)
 
-        print ("Row count of \(rowCount) needed for \(self.content.data.count) items.")
+        //print ("Row count of \(rowCount) needed for \(self.content.data.count) items.")
 
     }
 
     var body : some View {
+        
         VStack(alignment: .center, spacing: self.spacing) {
+            
             ForEach(0..<rowCount) { rowIndex in
+                
                 HStack (alignment: .center, spacing: self.spacing) {
+                    
                     ForEach((rowIndex * self.columns)..<(rowIndex * self.columns) + self.columns) { itemIndex in
                         if itemIndex >= self.content.data.count {
                             Text("")
-                                .frame(minWidth: 0, maxWidth: .infinity,
-                                       minHeight: 0, maxHeight: .infinity)
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                             
                         } else {
                             self.content.content(itemIndex)
-                                .frame(minWidth: 0, maxWidth: .infinity,
-                                       minHeight: 0, maxHeight: .infinity)
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                         }
                     }
-                }.frame(minWidth: 0, maxWidth: .infinity,
-                        minHeight: 0, maxHeight: .infinity)
+                    
+                }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                
             }
+            
         }
-        .frame(minWidth:0, maxWidth: .infinity,
-               minHeight:0, maxHeight: .infinity)
+        .frame(minWidth:0, maxWidth: .infinity, minHeight:0, maxHeight: .infinity)
+        
     }
-    
     
 }
 
